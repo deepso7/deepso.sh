48b0d8d9af2a26aa67e6e2d1c3a97ee5f7024744
Deepso <deeps.o7o8.y@gmail.com>
2026-08-07 19:06:46 +0530

    update deps

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -1,7 +1,7 @@
 // deno-fmt-ignore-file
 // biome-ignore format: generated types do not need formatting
 // prettier-ignore
-import type { PathsForPages, GetConfigResponse } from 'waku/router';
+import type { PathsForPages, GetConfigResponse, SearchCodecsForPages } from 'waku/router';
 
 // prettier-ignore
 import type { getConfig as File_Index_getConfig } from './pages/index';
@@ -14,11 +14,17 @@ type Page =
 | ({ path: '/writing/[slug]' } & GetConfigResponse<typeof File_WritingSlug_getConfig>);
 
 // prettier-ignore
+type Layout =
+| { path: '/' };
+
+// prettier-ignore
 declare module 'waku/router' {
   interface RouteConfig {
     paths: PathsForPages<Page>;
   }
   interface CreatePagesConfig {
     pages: Page;
+    layouts: Layout;
   }
+  interface SearchCodecsConfig extends SearchCodecsForPages<Page> {}
 }
